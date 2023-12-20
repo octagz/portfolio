0bf1f626f6ed1b79755428d4a1e8d9cc7881a54e
Octavio Gzain <octavio.gzain@digitalstrom.com>
2023-12-20 17:32:27 -0300

    fix padding on /contact

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -36,7 +36,7 @@ export default function Example() {
 	return (
 		<div className=" bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation currentPage="contact"/>
-			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto mb-32">
+			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto pb-12">
 				<div className="h-auto grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-0 sm:grid-cols-4 lg:gap-16">
 					{socials.map((s) => (
 						<Card>
